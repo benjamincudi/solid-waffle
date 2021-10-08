8fbbeea8b4e96204399d3615ea3536efb60bd273
Ben Garfield <bengarfield@gmail.com>
2021-10-07 23:42:09 -0400

    support creating sellers via POST
    - this can either be via form data or JSON,
    with the latter requiring the Content-Type be
    specified as application/json

--- api/handlers/sellers.go
+++ api/handlers/sellers.go
@@ -10,6 +10,7 @@ import (
 
 type sellersControllerDS interface {
 	GetSellers(ctx context.Context) ([]models.Seller, error)
+	InsertSeller(ctx context.Context, name string) (models.Seller, error)
 }
 
 type SellersController struct {
@@ -23,4 +24,26 @@ func (ctrl SellersController) HandleGet(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, sellers)
+}
+
+type sellerForm struct {
+	Name string `form:"name" json:"name"`
+}
+
+func (ctrl SellersController) HandlePost(c *gin.Context) {
+	var newSeller sellerForm
+	if err := c.ShouldBind(&newSeller); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "unable to bind seller form"})
+		return
+	}
+	if len(newSeller.Name) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "name must be non-empty string"})
+		return
+	}
+	seller, err := ctrl.DS.InsertSeller(c.Request.Context(), newSeller.Name)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, seller)
 }
